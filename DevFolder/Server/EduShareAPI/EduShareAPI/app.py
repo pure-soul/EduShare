281f7e83298bf9e7b6521e436d6bd8ad8086297e
Shemar Henry <shemarhenry24@yahoo.com>
2020-01-18 14:02:39 +0800

    Updated

--- DevFolder/Server/EduShareAPI/EduShareAPI/app.py
+++ DevFolder/Server/EduShareAPI/EduShareAPI/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, abort, make_response, render_template, redirec
 from content import items, users, api
 import html
 import urllib, json
-import requests
+#import requests
 
 app = Flask(__name__, template_folder='Forms')
 
