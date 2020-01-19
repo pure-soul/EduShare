c692442c6dd1a0a1d3195365e3a027578aa16990
Shemar Henry <shemarhenry24@yahoo.com>
2020-01-20 04:44:46 +0800

    Update chat.py

--- DevFolder/Server/EduShareAPI/EduShareAPI/chat.py
+++ DevFolder/Server/EduShareAPI/EduShareAPI/chat.py
@@ -1,11 +1,19 @@
 from flask import Flask
-from flask_socketio import SocketIO
+import socketio
+# from flask_socketio import SocketIO
 
-app = Flask(__name__, template_folder='Forms')
-app.config['SECRET_KEY'] = 'neVEraSkeDaNEgUFOsh!T,ThATiSSAfetOsAy'
-socketio = SocketIO(app)
+# app = Flask(__name__, template_folder='Forms')
+# app.config['SECRET_KEY'] = 'neVEraSkeDaNEgUFOsh!T,ThATiSSAfetOsAy'
+# socketio = SocketIO(app)
 
+sio = socketio.Server(async_mode='threading')
+app = Flask(__name__)
+app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
 
+# ... Socket.IO and Flask handler functions ...
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run(threaded=True)
+
+# if __name__ == '__main__':
+#     socketio.run(app)
