549e758b1dbc3bedc33c737887b98d9c6a627dee
Shemar Henry <shemarhenry24@yahoo.com>
2020-01-20 23:16:49 +0800

    Random

--- DevFolder/Server/EduShareAPI/EduShareAPI/Chat/chat.py
+++ DevFolder/Server/EduShareAPI/EduShareAPI/Chat/chat.py
@@ -30,3 +30,24 @@
 
 # # if __name__ == '__main__':
 # #     socketio.run(app)
+
+import socketio
+from bocadillo import App, configure, Templates, static
+
+app = App()
+configure(app)
+templates = Templates()
+
+sio = socketio.AsyncServer(async_mode="asgi")
+app.mount("/sio", socketio.ASGIApp(sio))
+
+@app.route("/")
+async def index(req, res):
+    res.html = await templates.render("index.html")
+
+@sio.on("message")
+async def broadcast(sid, data: str):
+    print("message:", data)
+    await sio.emit("response", data)
+
+app.mount("/socket.io", static("node_modules/socket.io-client/dist"))
