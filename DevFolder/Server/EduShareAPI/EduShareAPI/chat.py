05b40c5ff2078f8b2e8acc6f81b97f86fa255a81
Shemar Henry <shemarhenry24@yahoo.com>
2020-01-20 14:09:43 +0800

    Chat Implementations

--- DevFolder/Server/EduShareAPI/EduShareAPI/chat.py
+++ DevFolder/Server/EduShareAPI/EduShareAPI/chat.py
@@ -10,7 +10,20 @@ sio = socketio.Server(async_mode='threading')
 app = Flask(__name__)
 app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
 
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
+    raise ConnectionRefusedError('authentication failed')
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
 # ... Socket.IO and Flask handler functions ...
+
+@sio.event
+def send_message_to(sid, message, reciever):
+    pass
 
 if __name__ == '__main__':
     app.run(threaded=True)
