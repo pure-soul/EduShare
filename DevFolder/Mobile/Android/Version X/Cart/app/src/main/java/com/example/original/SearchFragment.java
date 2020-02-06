6a7f4607c835afb1828d392a91d14ca4abecd994
Shemar Henry <shemarhenry24@yahoo.com>
2020-02-06 20:04:01 +0800

    Added Some Java Code for Reference

---
+++ DevFolder/Mobile/Android/Version X/Cart/app/src/main/java/com/example/original/SearchFragment.java
@@ -0,0 +1,164 @@
+
+package com.example.original;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class SearchFragment extends Fragment {
+
+    private ArrayList<Item> mShoppingList = new ArrayList<>();
+    private RecyclerView mRecyclerView;
+    private SearchAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private Searcher mSearcher;
+    SearchView mSearch;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+
+        //search
+        mSearch = v.findViewById(R.id.searchb);
+        mSearch.performClick();
+        mSearch.setPressed(true);
+        mSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                filter(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                filter(s);
+                return false;
+            }
+
+        });
+
+        //Recycler view
+        mRecyclerView = v.findViewById(R.id.recylerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mAdapter = new SearchAdapter(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        filter("popular");
+
+        return v;
+    }
+
+    private void filter(String text){
+        if(mSearcher != null && mSearcher.getStatus() == AsyncTask.Status.RUNNING){
+            mSearcher.cancel(true);
+        }
+        mSearcher = (Searcher) new Searcher(text).execute();
+    }
+
+    // ASYNC TASK Used To Fetch List of Items According To Search
+    public class Searcher extends AsyncTask<Void, String, ArrayList<Item>> {
+
+//        private final String SEARCH_URL = "http://10.3.0.54:5000/supermercado/api/v1.0/search/"; //connecting <--commented
+        private final String SEARCH_URL = "http://192.168.3.126/edushare/api/v1.0/search/"; //connecting <--added
+        private String mStringData = "";
+        private String mSearchString;
+        public ArrayList<Item> RESULTS = new ArrayList<>();
+
+        public Searcher(String search) {
+            mSearchString = search.toLowerCase();
+        }
+
+        @Override
+        protected ArrayList<Item> doInBackground(Void... params) {
+            String JTAG = "EduShareJSON";
+            String search = SEARCH_URL + mSearchString;
+            try {
+                URL searchURL = new URL(search);
+                HttpURLConnection connection = (HttpURLConnection) searchURL.openConnection();
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line = "";
+                while (line != null) {
+                    line = reader.readLine();
+                    mStringData += line;
+                }
+
+                //JSONObject jsnobject = new JSONObject(mStringData); <-- commented
+                JSONArray resultARRAY = new JSONArray(mStringData); //jsnobject.getJSONArray(mSearchString); <-- edited
+                Log.i(JTAG, resultARRAY.toString());
+                for (int i = 0; i < resultARRAY.length(); i++) {
+                    Item item = new Item();
+                    JSONObject searchItem = (JSONObject) resultARRAY.get(i);
+                    Log.i(JTAG, searchItem.toString());
+                    item.setName(searchItem.getString("title"));
+                    Log.i(JTAG, searchItem.getString("title"));
+                    float cost = 0; // <--added
+                    int isle = 1; // <--added
+                    item.setCost(cost); // <--added
+                    item.setIsleNum(isle); // <--added
+//                    item.setCost((float) searchItem.getDouble("cost")); <-- commented
+//                    Log.i(JTAG, searchItem.getString("cost")); <--commented
+                    item.setAmount(searchItem.getInt("quantity"));
+                    Log.i(JTAG, searchItem.getString("quantity"));
+                    item.setCost((float) searchItem.getDouble("price"));
+                    Log.i(JTAG, searchItem.getString("price"));
+                    item.setImageURL(searchItem.getString("image"));
+                    Log.i(JTAG, searchItem.getString("image"));
+                    item.setID(searchItem.getString("id"));
+                    Log.i(JTAG, searchItem.getString("id"));
+                    item.setContent(searchItem.getString("content"));
+                    Log.i(JTAG, searchItem.getString("content"));
+//                    item.setIsleNum(searchItem.getInt("isle_no")); <-- commented
+//                    Log.i(JTAG, searchItem.getString("isle_no")); <--commented
+                    Log.i(JTAG, item.toString());
+                    this.RESULTS.add(item);
+                    Log.i(JTAG, RESULTS.toString());
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return RESULTS;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Item> items) {
+            super.onPostExecute(items);
+            mShoppingList = items;
+            mAdapter.addList(mShoppingList);
+        }
+    }
+}
--- DevFolder/Server/EduShareServices/EduShareAPI/content.py
+++ DevFolder/Server/EduShareServices/EduShareAPI/content.py
@@ -1,36 +1,88 @@
 items = [
+
+    # {
+    #     'id' : u'7459237505',
+    #     'title': u'Lasco Milk',
+    #     'content': u'Milk Powder by Lasco',
+    #     'price': 60.05,
+    #     'quantity': 100,
+    #     'islenum': 20,
+    #     'image': u'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjFKz_2QaOroxoCC11ed4Jmti3gIAMM765Hsu-Hnlvf2VikD4A7g',
+    #     'tags': u'#milk#lasco#lasco milk'
+    # },
+    # {
+    #     'id' : u'9859237505',
+    #     'title': u'Serge Milk',
+    #     'content': u'Milk by Serger',
+    #     'price': 100.05,
+    #     'quantity': 100,
+    #     'islenum': 20,
+    #     'image': u'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjFKz_2QaOroxoCC11ed4Jmti3gIAMM765Hsu-Hnlvf2VikD4A7g',
+    #     'tags' : u'#milk#serge#serge milk'
+    # },
+    # {
+    #     'id' : u'9369256538',
+    #     'title': u'Trix Cereal',
+    #     'content': u'Cereal by Trix',
+    #     'price': 110.05,
+    #     'quantity': 100,
+    #     'islenum': 20,
+    #     'image': u'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjFKz_2QaOroxoCC11ed4Jmti3gIAMM765Hsu-Hnlvf2VikD4A7g',
+    #     'tags': u'#cereal#trix#trix cereal'
+    # },
+    # {
+    #     'id' : u'73946844246',
+    #     'title': u'Grace Mackerel',
+    #     'content': u'Tin Macerel by Grace',
+    #     'price': 220.05,
+    #     'quantity': 100,
+    #     'islenum': 20,
+    #     'image': u'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjFKz_2QaOroxoCC11ed4Jmti3gIAMM765Hsu-Hnlvf2VikD4A7g',
+    #     'tags': u'#mackerel#grace#grace mackerel'
+    # },
+    # {
+    #     'id' : u'328754413',
+    #     'title': u'National Bread',
+    #     'content': u'Bread by NationPal Bakery',
+    #     'price': 220.05,
+    #     'quantity': 100,
+    #     'islenum': 20,
+    #     'image': u'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjFKz_2QaOroxoCC11ed4Jmti3gIAMM765Hsu-Hnlvf2VikD4A7g',
+    #     'tags': u'#bread#national#national bread#popular'
+    # }
+
     {
         'id' : u'7459237505',
-        'title': u'Lasco Milk',
-        'content': u'Milk Powder by Lasco',
+        'title': u'Lasco Milk and Your Health',
+        'content': u'A research paper Milk Powder by Lasco',
         'link': u'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjFKz_2QaOroxoCC11ed4Jmti3gIAMM765Hsu-Hnlvf2VikD4A7g',
         'tags': u'#milk#lasco#lasco milk'
     },
     {
         'id' : u'9859237505',
-        'title': u'Serge Milk',
-        'content': u'Milk by Serger',
+        'title': u'Is Serge Milk Good For You',
+        'content': u'A research paper Milk by Serger',
         'link': u'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjFKz_2QaOroxoCC11ed4Jmti3gIAMM765Hsu-Hnlvf2VikD4A7g',
         'tags' : u'#milk#serge#serge milk'
     },
     {
         'id' : u'9369256538',
-        'title': u'Trix Cereal',
-        'content': u'Cereal by Trix',
+        'title': u'Trix Cereal: Only For Kids?',
+        'content': u'A research paper Cereal by Trix',
         'link': u'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjFKz_2QaOroxoCC11ed4Jmti3gIAMM765Hsu-Hnlvf2VikD4A7g',
         'tags': u'#cereal#trix#trix cereal'
     },
     {
         'id' : u'73946844246',
-        'title': u'Grace Mackerel',
-        'content': u'Tin Macerel by Grace',
+        'title': u'10 days with Grace, Mackerel',
+        'content': u'A research paper Tin Macerel by Grace',
         'link': u'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjFKz_2QaOroxoCC11ed4Jmti3gIAMM765Hsu-Hnlvf2VikD4A7g',
         'tags': u'#mackerel#grace#grace mackerel'
     },
     {
         'id' : u'328754413',
-        'title': u'National Bread',
-        'content': u'Bread by NationPal Bakery',
+        'title': u'Is National Bread Key',
+        'content': u'A research paper Bread by NationPal Bakery',
         'link': u'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjFKz_2QaOroxoCC11ed4Jmti3gIAMM765Hsu-Hnlvf2VikD4A7g',
         'tags': u'#bread#national#national bread#popular'
     }
