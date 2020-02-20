1028bf6044afe8db530a789dd1a3f59d8627ee87
envaleed <mark_2k9@live.com>
2020-02-20 14:30:05 +0800

    Update DBScript.sh

--- DevFolder/DBMS/EduShareDBMS/PythonApplication1/DBScript.sh
+++ DevFolder/DBMS/EduShareDBMS/PythonApplication1/DBScript.sh
@@ -1,3 +1,7 @@
-open -a Docker
-cd  /Users/shemarhenry/Desktop/EduShare/DevFolder/DBMS/EduShareDBMS/PythonApplication1
-docker build -t edusharedb .
+AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
+AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
+
+docker build -t my_app .
+docker run -it --rm \
+   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
